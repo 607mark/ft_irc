a19083d74b14c4525fbccd02fac405d457a1ca90
607mark <40577872+607mark@users.noreply.github.com>
2025-09-08 23:08:55 +0300

    init commit in invite branch

---
+++ src/inputHandle.cpp
@@ -0,0 +1,64 @@
+#include "irc.hpp"
+#include "commands/commands.hpp"
+
+std::string handleInput(const std::string &input, Server *server, int clientFd)
+{
+	std::string result = "";
+	try
+	{
+
+		std::vector<std::string> args = split(input, ' ');
+		if (args.empty())
+			return "";
+
+		if (args.at(0).starts_with('/'))
+		{
+			args.at(0).erase(args.at(0).begin());
+		}
+
+		std::string cmdLowercase = strToLowercase(args.at(0));
+
+		validateCommand(cmdLowercase);
+
+		Client &client = server->getClients().at(clientFd);
+
+		switch (hash(cmdLowercase.c_str(), cmdLowercase.size()))
+		{
+		case hash("nick"):
+			if (args.size() < 2)
+				result = "431 :No nickname given\r\n";
+			else
+				result = handleNick(client, args.at(1));
+			break;
+
+		case hash("user"):
+			if (args.size() < 5) // RFC requires 4 params
+				result = "461 USER :Not enough parameters\r\n";
+			else
+				result = handleUser(client, args);
+			break;
+
+		case hash("join"):
+			std::cout << "joining..." << args.at(1) << std::endl;
+			result = handleJoin(server, args, client);
+			break;
+		case hash("invite"):
+			if (args.size() < 3)
+				result = "461 INVITE :Not enough parameters\r\n";
+			else
+			{
+				std::cout << "inviting..." << args.at(1) << std::endl;
+				result = handleInvite(server, args, client);
+			}
+			break;
+
+		default:
+			break;
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return result;
+}
